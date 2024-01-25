58acfe0eee21708fc8144ab83094449f074eb3a5
romullopc <romullo.exe@outlook.com>
2024-01-25 11:52:02 -0300

    atualizando exercícios de estrutura condicional + Js + html + css.

---
+++ estrutura_condicional/simples.js
@@ -0,0 +1,11 @@
+
+var vel = 80
+
+console.log(`a velocidade do seu carro é ${vel} km/h`);
+
+
+if (vel > 60) {      //Condição Simples!
+    console.log(`você esta acima do limite de velocidade!`);
+}
+
+
